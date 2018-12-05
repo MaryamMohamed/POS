a768cf0e4ee537dd4c33b3b7224263c460914443
MaryamMohamed <40955459+MaryamMohamed@users.noreply.github.com>
2018-12-05 02:56:10 +0200

    core

---
+++ Employee.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author 1300180
+ */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*public class Employee extends Person{
+    
+    static ArrayList<Employee> Arr = new ArrayList<Employee>();
+    static int count = -1;
+    static boolean flage = true;
+    int x= 5;
+    
+    int age;
+    double salary;
+    
+    
+    
+    
+    public Employee() {
+    }
+    public Employee(int id, String fname, String lname, int age, int level, double GPA) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.level = level;
+        this.GPA = GPA;
+    }
+
+    
+}
+*/
